c60558dc73a914db2741271bac507ba3ca543e38
Michael Eckstine <michaeleckstine@KhaledsiPhone7.localdomain>
2019-06-03 13:43:27 -0600

    fixed insert query

--- sql/query/insert.sql
+++ sql/query/insert.sql
@@ -4,8 +4,8 @@ VALUES ('Michael', 'Eckstine', 21, 'Stir Fried Steak and Veggies');
 INSERT INTO People (FirstName, LastName, Age, FavoriteFood)
 VALUES ('Delete', 'Me', 99, 'Paper/Wood Pulp');
 
-INSERT INTO People (FirstName, Age, FavoriteFood)
-VALUES ('Unknown', 9999999999, 'Souls');
+INSERT INTO People (FirstName, LastName, Age, FavoriteFood)
+VALUES ('Unknown', 'Unknown', 999, 'Souls');
 
 INSERT INTO People (FirstName)
 VALUES ('Blank');
