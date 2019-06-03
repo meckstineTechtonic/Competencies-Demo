f13b354dee0159c2bfd0c9312d259e51a4e7318a
Michael Eckstine <michaeleckstine@KhaledsiPhone7.localdomain>
2019-06-03 15:20:53 -0600

    update

--- sql/query/update.sql
+++ sql/query/update.sql
@@ -3,5 +3,5 @@ SET FirstName='Mikey', LastName='Mikey', Age=42, FavoriteFood='Dirt'
 WHERE Age IS NULL; 
 
 UPDATE People
-SET FirstName='Cthulu'
+SET FirstName='Cthulu', FavoriteFood='Souls'
 WHERE Age=999; 
